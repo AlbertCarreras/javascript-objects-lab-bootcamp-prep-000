6fe0bd188e1364c27422bd0369f7c5f63e1ec85b
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 19:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes = new Object ({});
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]: value})
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  Object.assign(object, {[key]: value})
+  return object
 }
