5f9a3cadea97de7312a714766345c6ee1c68c1f7
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 19:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  delete Object.assign({},object)[key]
-  return object
+  return delete Object.assign({},object)[key]
+  
 }
